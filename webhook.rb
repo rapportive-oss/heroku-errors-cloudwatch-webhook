f3adb8adc1f81d96579ce18296d455b93584af7a
Sam Stokes <me@samstokes.co.uk>
2011-04-01 11:56:25 -0700

    Parse and pretty-print

--- webhook.rb
+++ webhook.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
+require 'cgi'
+require 'json'
+require 'pp'
+
 get '/' do
   'This is a web hook!'
 end
 
 post '/' do
-  puts request.body.read
+  pp JSON.parse(CGI.unescape(request.body.read))
   'OK'
 end
